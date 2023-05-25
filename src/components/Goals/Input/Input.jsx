921df09fdc19d81e9a1c3bb8ee8604363fd946c9
“crlnvls” <cveloso@hotmail.co.uk>
2023-05-25 14:00:57 +0100

    Add dynamic css class

--- src/components/Goals/Input/Input.jsx
+++ src/components/Goals/Input/Input.jsx
@@ -5,21 +5,40 @@ import "./Input.css";
 
 const Input = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+  const goalInputChangeHandler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(e.target.value);
   };
 
-  const formSubmitHandler = (event) => {
-    event.preventDefault();
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label
+        // style={{ color: !isValid ? "red" : "black" }}
+        >
+          Course Goal
+        </label>
+        <input
+          // style={{
+          //   borderColor: !isValid ? "red" : "#ccc",
+          //   backgroundColor: !isValid ? "salmon" : "transparent",
+          // }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
